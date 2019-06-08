223469cf9d8add0801f5cdfd192bb1b68282b1b2
Pavel Solodovnikov <pavel.al.solodovnikov@gmail.com>
2019-06-08 03:02:51 +0300

    Switch to in-memory sorting instead of moving pointers to pages and writing via iovec's.
    Although, it's much slower and introduces reactor stalls. Need to
    investigate further.
    
    Un-hardcode memory limits for in-mem reading and sorting.

--- apps/external_merge_sort/main.cc
+++ apps/external_merge_sort/main.cc
@@ -7,94 +7,87 @@
 #include <seastar/core/file.hh>
 #include <seastar/core/file-types.hh>
 #include <seastar/util/log.hh>
+#include <seastar/core/units.hh>
 
 #include <fmt/printf.h>
 #include <boost/range/irange.hpp>
 
-#include <sys/uio.h>
 #include <algorithm>
+
+#include <array>
 
 namespace bpo = boost::program_options;
 
-constexpr std::size_t record_size = 1 << 12; // 4096 bytes
-constexpr std::size_t records_per_partition = 10000; // Approx. 40MB
+constexpr std::size_t record_size = 4 * seastar::KB;
+using record_type = std::array<char, record_size>;
 
 seastar::logger task_logger("external_merge_sort");
 
-struct iovec_compare
+struct record_compare
 {
-    bool operator()(iovec const& lhs, iovec const& rhs) const
+    bool operator ()(record_type const& lhs, record_type const& rhs) const
     {
-       auto lhs_base = reinterpret_cast< unsigned char* >( lhs.iov_base ),
-            rhs_base = reinterpret_cast< unsigned char* >( rhs.iov_base );
-       return std::lexicographical_compare(lhs_base, lhs_base + record_size,
-                                           rhs_base, rhs_base + record_size);
+        return std::lexicographical_compare(lhs.data(), lhs.data() + record_size,
+                                            rhs.data(), rhs.data() + record_size);
     }
 };
 
-std::vector< iovec > sort_raw_data(seastar::temporary_buffer<unsigned char> const& buf)
+seastar::future<> sort_raw_data(seastar::temporary_buffer<char>& buf)
 {
-    std::vector< iovec > result;
-    result.reserve(buf.size() / record_size);
-    for(auto pos = buf.begin(), end = buf.end(); pos != end; pos += record_size)
-        result.push_back(iovec{const_cast< unsigned char* >( &*pos ), record_size});
-    std::sort(result.begin(), result.end(), iovec_compare());
-    return result;
+    char* raw_write_ptr = buf.get_write();
+    record_type* aliased_write_ptr = reinterpret_cast< record_type* >(raw_write_ptr),
+        *aliased_end_ptr = reinterpret_cast< record_type* >(raw_write_ptr + buf.size());
+    std::sort(aliased_write_ptr, aliased_end_ptr, record_compare());
+    return seastar::make_ready_future<>();
 }
 
 class PartitionerService {
-    seastar::file mFile;
-    std::uint32_t mCurrentPartitionId;
-    std::uint64_t mPartitionCount;
-
-    std::size_t mLeft;
-    std::size_t mWritePos;
+    seastar::file mInputFile;
+    std::size_t mAlignedCpuMemSize;
+    std::size_t mCurrentPartitionId;
+    std::size_t mPartitionsCount;
 
 public:
 
-    PartitionerService(seastar::file_handle fd)
-        : mFile(fd.to_file()),
+    PartitionerService(seastar::file_handle fd, std::size_t input_fd_size, size_t cpu_memory)
+        : mInputFile(fd.to_file()),
+          mAlignedCpuMemSize(cpu_memory / record_size * record_size), // align to be a multiple of `record_size`
           mCurrentPartitionId(seastar::engine().cpu_id()),
-          mPartitionCount(mFile.size().get0() / record_size / records_per_partition)
+          mPartitionsCount(input_fd_size / mAlignedCpuMemSize)
     {}
 
-    seastar::future<> stop() { return seastar::make_ready_future<>(); }
+    seastar::future<> stop() {
+        return seastar::make_ready_future<>();
+    }
 
     seastar::future<> start() {
-        return seastar::do_until([this] { return mCurrentPartitionId >= mPartitionCount; }, [this] {
-            mLeft = record_size * records_per_partition;
-            mWritePos = 0;
-            auto const bytes_to_read = record_size * records_per_partition;
-            task_logger.info("reading {} bytes from input file. Offset {}", bytes_to_read, mCurrentPartitionId * bytes_to_read);
-            return mFile.dma_read<unsigned char>(mCurrentPartitionId * bytes_to_read, bytes_to_read)
+        return seastar::do_until([this] { return mCurrentPartitionId >= mPartitionsCount; }, [this] {
+            task_logger.info("reading {} bytes from input file. Offset {}", mAlignedCpuMemSize, mCurrentPartitionId * mAlignedCpuMemSize);
+            return mInputFile.dma_read<char>(mCurrentPartitionId * mAlignedCpuMemSize, mAlignedCpuMemSize)
                 .then([this](auto buf) {
-                    return create_partition(std::move(buf));
+                    task_logger.info("successfully read data");
+                    return create_initial_partition(std::move(buf));
                 });
         });
     }
 
 private:
 
-    seastar::future<> create_partition(seastar::temporary_buffer<unsigned char> buf)
+    seastar::future<> create_initial_partition(seastar::temporary_buffer<char> buf)
     {
         return seastar::do_with(std::move(buf), [&] (auto& buf) {
-            return seastar::do_with(sort_raw_data(buf), [&](auto& sorted_buf) {
-                return seastar::open_file_dma("/opt/test_data/partition" + seastar::to_sstring(mCurrentPartitionId),
+            return sort_raw_data(buf).then([&] {
+                return seastar::open_file_dma("/opt/test_data/level0-" + seastar::to_sstring(mCurrentPartitionId),
                     seastar::open_flags::create | seastar::open_flags::truncate | seastar::open_flags::wo)
                     .then([&](seastar::file output_partition_fd) {
                         return seastar::do_with(std::move(output_partition_fd), mCurrentPartitionId, [&](auto& output_partition_fd, auto& prev_partition_id) {
                             task_logger.info("writing to partition {}", prev_partition_id);
                             mCurrentPartitionId += seastar::smp::count;
-                            return seastar::do_until([this] { return mLeft == 0; }, [&] {
-                                return output_partition_fd.dma_write(mWritePos, sorted_buf)
-                                    .then([&](size_t s) {
-                                        task_logger.info("successfully written {} bytes to partition {}", s, prev_partition_id);
-                                        mWritePos += s;
-                                        mLeft -= s;
-                                        sorted_buf.erase(sorted_buf.begin(), sorted_buf.begin() + s / record_size);
-                                        return seastar::make_ready_future<>();
+                            return output_partition_fd.dma_write(0u, buf.get(), buf.size())
+                                .then([&](size_t s) {
+                                    task_logger.info("successfully written {} bytes to partition {}", s, prev_partition_id);
+                                    return seastar::make_ready_future<>();
                                 });
-                            });
                         });
                     });
             });
@@ -111,22 +104,32 @@ int main(int argc, char** argv) {
     seastar::sharded<PartitionerService> partitioner;
     return app.run(argc, argv, [&] {
         return seastar::async([&] {
+            std::size_t const available_memory = seastar::memory::stats().free_memory() / 2,
+                per_cpu_memory = available_memory / seastar::smp::count;
+            fmt::print("Number of cores: {}\n"
+                       "Total available memory: {} bytes ({} MB)\n"
+                       "Memory available to each core: {} bytes ({} MB)\n",
+                       seastar::smp::count,
+                       available_memory, available_memory / 1024 / 1024,
+                       per_cpu_memory, per_cpu_memory / 1024 / 1024);
+
             auto& opts = app.configuration();
             seastar::sstring const& input_filepath = opts["input"].as<seastar::sstring>();
             auto input_fd = seastar::open_file_dma(input_filepath, seastar::open_flags::ro).get0();
+            std::size_t const input_size = input_fd.size().get0();
 
-            std::uint64_t const partition_count = input_fd.size().get0() / record_size / records_per_partition;
-            fmt::print("overall partitions count: {}\n", partition_count);
-
-            partitioner.start(input_fd.dup()).get0();
+            partitioner.start(input_fd.dup(), input_size, per_cpu_memory).get0();
             seastar::engine().at_exit([&partitioner] {
                 return partitioner.stop();
             });
+
             // initial partitioning pass
             partitioner.invoke_on_all([] (auto& p) {
                 return p.start();
             }).get0();
             input_fd.close().get0();
+
+            // repeatedly invoke merge passes until there is nothing to merge
         });
     });
 }
